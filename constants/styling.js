0cb42f2a7345b91bc7e2d8b5960638ae0fded01b
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 15:22:52 -0700

    Add Styling & Sizing

--- constants/styling.js
+++ constants/styling.js
@@ -1,8 +1,14 @@
 export const colors = {
   primary: '#0040b8',
   secondary: '#cacaca',
-  tertiary: '#000'
+  tertiary: '#000',
+  additionalColor1: '#02215c',
+  additionalColor2: '#616161'
 };
 
-const theme = { colors };
+export const sizes = {
+  setupSize: 90,
+};
+
+const theme = { colors, sizes };
 export default theme;

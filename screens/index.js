9764b22e1dfa22597299249b5d5ab65afab82896
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 09:22:04 -0700

    Add Index for Screens

---
+++ screens/index.js
@@ -0,0 +1,15 @@
+import Home from './Home';
+import Landing from './Landing';
+import Product from './Product';
+import Results from './Results';
+import Settings from './Settings';
+import Setup from './Setup';
+
+export {
+  Home,
+  Landing,
+  Product,
+  Results,
+  Settings,
+  Setup
+};

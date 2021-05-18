38e78eaad24ea2775108afc31d8194564846e83a
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 10:30:07 -0700

    Add Search to Screens Router

--- screens/index.js
+++ screens/index.js
@@ -2,6 +2,7 @@ import Home from './Home';
 import Landing from './Landing';
 import Product from './Product';
 import Results from './Results';
+import Search from './Search';
 import Settings from './Settings';
 import Setup from './Setup';
 
@@ -10,6 +11,7 @@ export {
   Landing,
   Product,
   Results,
+  Search,
   Settings,
   Setup
 };

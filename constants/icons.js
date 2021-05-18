68ce1e38ea3d49d7d6553d1a41409b3a3c4445f8
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 10:29:09 -0700

    Add Icons to Constants

---
+++ constants/icons.js
@@ -0,0 +1,11 @@
+export const home = require ('../assets/icons/navbar/home.png');
+export const search = require ('../assets/icons/navbar/search.png');
+export const settings = require ('../assets/icons/navbar/settings.png');
+export const user = require ('../assets/icons/navbar/user.png');
+
+export default {
+  home,
+  search,
+  settings,
+  user
+};

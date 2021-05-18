57f28cb727beb3d17cb596096c601a37c4138341
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 09:12:49 -0700

    Set up base pages

---
+++ screens/Setup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Setup = () => {
+  return (
+    <View>
+      <Text>Setup</Text>
+    </View>
+  );
+};
+
+export default Setup;

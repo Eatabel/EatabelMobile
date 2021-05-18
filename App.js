5bbd1638e5ace5b22fb460eac634be42b5c52b51
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 09:30:14 -0700

    Move Landing Page to screens/landing

--- App.js
+++ App.js
@@ -1,52 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
-import { Icon } from 'react-native-elements';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Landing } from './screens';
+const Stack = createStackNavigator();
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    'IkarosBold': require('./assets/fonts/Ikaros-Regular-400.otf'),
-    'IkarosLight': require('./assets/fonts/Ikaros-Light-300.otf'),
-  });
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        start={{x: 1, y: 0}} //here we are defined x as start position
-        end={{x: 0, y: 0}} //here we can define axis but as end position
-        colors={['#3393E4', '#715886']}
-        style={{
-          height: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 5,
-        }}>
-        <Text style={styles.Heading}>Eatabel</Text>
-        <Text style={styles.SubHeading}>A Nutritional App</Text>
-        <Icon color='#fff' name='search' onPress={() => console.log('hello')} size="30"/>
-      </LinearGradient>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"Landing"}
+      >
+        <Stack.Screen name="Landing" component={Landing} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  linearGradient: {
-    flex: 1,
-  },
-  Heading: {
-    fontFamily: 'IkarosBold',
-    color: 'white',
-    fontSize: 80,
-  },
-  SubHeading: {
-    fontFamily: 'IkarosLight',
-    color: 'white',
-    fontSize: 30,
-  },
-  container: {
-    flex: 1,
-  },
-});
--- screens/Landing.js
+++ screens/Landing.js
@@ -1,12 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { Icon } from 'react-native-elements';
 
 const Landing = () => {
+  let [fontsLoaded] = useFonts({
+    'IkarosBold': require('../assets/fonts/Ikaros-Regular-400.otf'),
+    'IkarosLight': require('../assets/fonts/Ikaros-Light-300.otf'),
+  });
   return (
-    <View>
-      <Text>Landing</Text>
+    <View style={styles.container}>
+      <LinearGradient
+        start={{x: 1, y: 0}}
+        end={{x: 0, y: 0}}
+        colors={['#3393E4', '#715886']}
+        style={{
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 5,
+        }}>
+        <Text style={styles.Heading}>Eatabel</Text>
+        <Text style={styles.SubHeading}>A Nutritional App</Text>
+        <Icon color='#fff' name='search' onPress={() => console.log('hello')} size="30"/>
+      </LinearGradient>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+  Heading: {
+    fontFamily: 'IkarosBold',
+    color: 'white',
+    fontSize: 80,
+  },
+  SubHeading: {
+    fontFamily: 'IkarosLight',
+    color: 'white',
+    fontSize: 30,
+  },
+  container: {
+    flex: 1,
+  },
+});
 
 export default Landing;

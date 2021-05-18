a58577b06e93edc7670a49b6592dc9e42b43bd8e
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-18 15:22:16 -0700

    Add New Foods

---
+++ constants/foods.js
@@ -0,0 +1,9 @@
+export const dietaryMappings = [
+  {Poultry: [{Chicken: 'chicken'}, {Turkey: 'turkey'}, {Duck: 'duck'}]},
+  {Meat: [{Beef: 'cow'}, {Pork: 'pig'}]},
+  {Seafood: [{Fish: 'fish'}, {Shellfish: 'shrimp'}]},
+  {Other: [{Eggs: 'eggs'}, {Milk: 'milk'}]},
+];
+
+const foods = {dietaryMappings};
+export default foods;
--- constants/icons.js
+++ constants/icons.js
@@ -3,9 +3,29 @@ export const search = require ('../assets/icons/navbar/search.png');
 export const settings = require ('../assets/icons/navbar/settings.png');
 export const user = require ('../assets/icons/navbar/user.png');
 
+export const chicken = require ('../assets/icons/dietary/chicken.png');
+export const cow = require ('../assets/icons/dietary/cow.png');
+export const duck = require ('../assets/icons/dietary/duck.png');
+export const eggs = require ('../assets/icons/dietary/eggs.png');
+export const fish = require ('../assets/icons/dietary/fish.png');
+export const milk = require ('../assets/icons/dietary/milk.png');
+export const pig = require ('../assets/icons/dietary/pig.png');
+export const shrimp = require ('../assets/icons/dietary/shrimp.png');
+export const turkey = require ('../assets/icons/dietary/turkey.png');
+
+
 export default {
   home,
   search,
   settings,
-  user
+  user,
+  chicken,
+  cow,
+  duck,
+  eggs,
+  fish,
+  milk,
+  pig,
+  shrimp,
+  turkey
 };

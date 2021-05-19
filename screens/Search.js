bb3b6cde51b807b74ce54b1e62d66056f19e3107
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-19 09:56:04 -0700

    Implement Rough Barcode Scanner

--- screens/Search.js
+++ screens/Search.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { AsyncStorage, Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { colors, icons, fontMappings } from '../constants';
 
-const Search = () => {
+export default function Search ({navigation}) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    AsyncStorage.setItem('currentUPC', data)
+    navigation.navigate('Product')
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
-};
-
-export default Search;
+}
+const styles = StyleSheet.create({
+  searchBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    backgroundColor: colors.secondary,
+    borderRadius: 30,
+    width: '75%',
+  },
+  searchText: {
+    fontSize: 15,
+    color: colors.primary,
+    fontFamily: 'BrandonBold',
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    height: 30,
+  },
+});

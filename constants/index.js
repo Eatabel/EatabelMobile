ca81d8be763e6498b4018c3275dcef16cf0ea470
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-19 10:06:26 -0700

    Move Ingredient Mappings to Constants Folder

--- constants/index.js
+++ constants/index.js
@@ -2,4 +2,5 @@ import icons from './icons';
 import {colors, sizes} from './styling';
 import fontMappings from './fonts';
 import { dietaryMappings } from './foods';
-export { icons, colors, dietaryMappings, fontMappings, sizes};
+import { restrictedMappings } from './mappings';
+export { icons, colors, dietaryMappings, fontMappings, restrictedMappings, sizes};
---
+++ constants/mappings.js
@@ -0,0 +1,13 @@
+export const restrictedMappings = {
+  Beef: ['beef', 'gelatin', 'tallow'],
+  Pork: ['pork', 'lard', 'gelatin'],
+  Chicken: ['chicken'],
+  Turkey: ['turkey'],
+  Duck: ['duck'],
+  Fish: ['fish', 'salmon', 'tuna', 'pollock', 'cod', 'tilapia', 'anchovies', 'anchovy', 'oyster', 'clam'],
+  Shellfish: ['shrimp', 'langoustine', 'lobster', 'crab'],
+  Eggs: ['eggs', 'egg', 'albumen'],
+  Milk: ['milk', 'lactose', 'whey']
+};
+
+export default restrictedMappings;

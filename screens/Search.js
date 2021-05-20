33c85ef2f2c63ea60e941d9b860801a27d3ad24f
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-20 09:50:04 -0700

    Make Scan Again Text Viewable

--- screens/Search.js
+++ screens/Search.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AsyncStorage, Text, View, StyleSheet, Button } from 'react-native';
+import { AsyncStorage, Text, View, StyleSheet, Button, SafeAreaView } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { colors, icons, fontMappings } from '../constants';
 
@@ -17,7 +17,12 @@ export default function Search ({navigation}) {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     AsyncStorage.setItem('currentUPC', data)
-    navigation.navigate('Product')
+    .then(() => {
+      AsyncStorage.setItem('foodLookup', 'upc')
+    })
+    .then(() => {
+      navigation.navigate('Product')
+    })
   };
 
   if (hasPermission === null) {
@@ -28,34 +33,24 @@ export default function Search ({navigation}) {
   }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
+      {scanned && <Text style={styles.scanAgain} onPress={() => setScanned(false)}>Tap To Scan Again</Text>}
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  searchBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    backgroundColor: colors.secondary,
-    borderRadius: 30,
-    width: '75%',
-  },
-  searchText: {
-    fontSize: 15,
-    color: colors.primary,
-    fontFamily: 'BrandonBold',
-  },
   container: {
     alignItems: 'center',
     flex: 1,
     height: 30,
   },
+  scanAgain: {
+    fontFamily: 'BrandonMedium',
+    fontSize: 15,
+    color: colors.primary,
+  }
 });
